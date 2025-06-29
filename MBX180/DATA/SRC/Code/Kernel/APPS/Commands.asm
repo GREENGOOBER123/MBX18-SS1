b0af26cfc1d92c2a567606e71f63e4fe262c5ad4
MBX18 SS1 <Ethanprone@gmail.com>
2025-06-29 20:43:32 +0300

    Update Commands.asm

--- MBX180/DATA/SRC/Code/Kernel/APPS/Commands.asm
+++ MBX180/DATA/SRC/Code/Kernel/APPS/Commands.asm
@@ -3,7 +3,7 @@ DB Commands
 Section .text
 MAINDEFINE:
 	mov MAINDEFINE, SYSTEM
-	%include 'FILE SYSTEM.ASM':
+	%%include 'FILE SYSTEM.bin':
 	%define DIR, FAT16
 echo:
 	in KEYBOARD ;takes input from KEYBOARD
@@ -21,8 +21,9 @@ START:
 	in PROGRAM
 	call(PROGRAM)
 COMPILE:
-	%include 'Compiler.ASM'
+	%%include 'Compiler.bin'
 	call COMPILE
+	STOS COMPILE_FILE
 Copy:
 	mul FILE = 2, FILE_1, FILE_2
 	mov FILE_2 , in
@@ -43,14 +44,31 @@ LS:
 	mov si, Directory
 	mov ah, 0x0E
 	message db DIR
+SussyAI:
+	mov si , MSG
+	mov ah , 0x0E
+	MSG DB, "Okay if you say so child",0
+	mov di 0,1
+	MSG DB , "idk why",0
+	call(SussyAI)
 CD:
 	mov KERNEL, in
+MAKECAB:
+	%%include 'MAKECAB.bin'
+	%define "BUILD_DIR"
+	COMPILE_FILE in,in
+	out BINARY_FILE
 Clear:
 	cli
 	hlt Shell
 	int
 MKDIR:
 	STI
-	new
+	int 21h , Directory
+	in si
+	%define NAME in si , Directory
+MAKECOM:
+	%%include 'MAKECOM.bin'
+	call .INCR
 Section end
 STOSB .TEXT COMMANDS
